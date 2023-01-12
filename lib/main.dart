ff13414e2a6f0754ceb8c5c8cd4d17380dfa3c06
Rustam-Asdanov <asdanovrustam@gmail.com>
2023-01-12 17:05:15 +0100

    Some updates

---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/widgets.dart';
+
+void main(){
+  runApp(new MyApp());
+}
+
+class MyApp extends StatefulWidget{
+  @override
+  State<StatefulWidget> createState() {
+    // TODO: implement createState
+    throw UnimplementedError();
+  }
+
+}
