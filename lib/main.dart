cce9eb0741d5d01dc5f292a6b37310e6408e23d3
Rustam-Asdanov <asdanovrustam@gmail.com>
2023-01-12 17:49:47 +0100

    some updates

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,44 @@
+import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 
-void main(){
+void main() {
   runApp(new MyApp());
 }
 
-class MyApp extends StatefulWidget{
+class MyApp extends StatefulWidget {
   @override
   State<StatefulWidget> createState() {
-    // TODO: implement createState
-    throw UnimplementedError();
+    return _MyAppState();
   }
+}
 
+class _MyAppState extends State<MyApp> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text("Flutter App"),
+        ),
+        body: Column(
+          children: const <Widget>[
+            SizedBox(
+              width: 100,
+              child: Card(
+                color: Colors.blue,
+                elevation: 5,
+                child: SizedBox(
+                  width: double.infinity,
+                  child: Text("Chart!"),
+                ),
+              ),
+            ),
+            Card(
+              child: Text("List of TX"),
+            )
+          ],
+        ),
+      ),
+    );
+  }
 }
